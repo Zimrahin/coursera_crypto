dd3402339076c65df374e6584fae5e7844c9472c
Zimrahin <diegobadillosj@gmail.com>
2025-05-08 17:59:05 +0200

    Add more example lists quiz 2

--- week_2/quiz_2.py
+++ week_2/quiz_2.py
@@ -8,6 +8,12 @@ if __name__ == "__main__":
         (0x4AF53267_1351E2E1, 0x87A40CFA_8DD39154),
         (0x2D1CFA42_C0B1D266, 0xEEA6E3DD_B2146DD0),
     ]
+    outputs: list[tuple] = [
+        (0xE86D2DE2_E1387AE9, 0x1792D21D_B645C008),
+        (0x5F67ABAF_5210722B, 0xBBE033C0_0BC9330E),
+        (0x7C2822EB_FDC48BFB, 0x325032A9_C5E2364B),
+        (0x7B50BAAB_07640C3D, 0xAC343A22_CEA46D60),
+    ]
 
     for left, right in outputs:
         pattern = left ^ right
